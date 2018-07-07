9bfba29a66353c8060b557f1cd9a7d4b981bf5dd
Troy Neubauer <troyneubauer@gmail.com>
2018-07-06 22:32:13 -0400

    Ready for the hacktahon!!

---
+++ desktop/src/com/fish/notes/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fish.notes.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fish.notes.Notes;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Notes(), config);
+	}
+}
