b3a18e231fefd2bac3419d84965cb5179d2d2e68
J Kim <jjkimto@gmail.com>
2018-07-07 12:03:50 -0400

    Made all screen classes

---
+++ core/src/com/fish/notes/MakeScreen.java
@@ -0,0 +1,6 @@
+package com.fish.notes;
+
+import com.badlogic.gdx.Screen;
+
+public class MakeScreen implements Screen {
+}
---
+++ core/src/com/fish/notes/SettingsScreen.java
@@ -0,0 +1,6 @@
+package com.fish.notes;
+
+import com.badlogic.gdx.Screen;
+
+public class SettingsScreen implements Screen {
+}
