5915fdf9f81f20eb8eaef39f3f0941df4e9dcd39
J Kim <jjkimto@gmail.com>
2018-07-07 19:40:19 -0400

    THing

--- core/src/com/fish/notes/Notes.java
+++ core/src/com/fish/notes/Notes.java
@@ -31,11 +31,7 @@ public class Notes extends Game {
 		skin = new Skin(Gdx.files.internal("default.json"));
         addCreators();
         account = new Account(12546772, "testuser", new byte[0], "testeremail@tester.com");
-<<<<<<< Updated upstream
         setScreen(new ProfileScreen(this));
-=======
-        setScreen(new ShopScreen(this));
->>>>>>> Stashed changes
     }
 
 	public static void showDialog(String title, String message) {
