da5f50e22eb766043fa9a1b844255b9305526be9
Mahi Nair <mahifreya@gmail.com>
2018-07-07 11:09:54 -0400

    Mahi's first commit

--- core/src/com/fish/notes/Notes.java
+++ core/src/com/fish/notes/Notes.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class Notes extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	public static Skin skin;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Fishnotes.png");
+		//skin =
 	}
 
 	@Override
