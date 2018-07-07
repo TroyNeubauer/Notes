33958f287f6f61c6d673995a87d10edb64a69422
J Kim <jjkimto@gmail.com>
2018-07-07 12:59:11 -0400

    Auto stash before merge of "master" and "origin/master"

--- core/src/com/fish/notes/MakeScreen.java
+++ core/src/com/fish/notes/MakeScreen.java
@@ -1,33 +1,45 @@
 package com.fish.notes;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-
-import java.awt.TextField;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import de.tomgrill.gdxdialogs.core.dialogs.GDXButtonDialog;
 
 public class MakeScreen implements Screen {
     private Stage stage;
-    private TextButton loginButton, registerBtn;
+    private TextButton sumbit, picchoice, textchoice;
     private Label label;
     private TextField title;
-
     private Notes game;
-    public MakeScreen() {
+
+
+    public MakeScreen(final Notes game) {
         this.game = game;
         this.stage = stage;
 
-
+    
         this.title = new TextField("", Notes.skin);
         title.setMessageText("Title");
 
         container.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Table container = new Table();
         container.add(title);
+
+        this.sumbit = new TextButton("Post", Notes.skin);
+        sumbit.addListener(finalize);
+
+        EventListener finalize = new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                //Post temp = new Post(new PostData post, title.getText());
+                return false;
+            }
+        }
     }
+
+
 
     public void render(float delta) {
         stage.act(delta);
@@ -37,5 +49,8 @@ public class MakeScreen implements Screen {
         stage.dispose();
     }
 
+    @Override
+    public void show() {
 
+    }
 }
