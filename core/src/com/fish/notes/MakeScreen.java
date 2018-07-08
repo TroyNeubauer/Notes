280d4a423b99241093c0c405d18c5921710fd99c
Mahi Nair <mahifreya@gmail.com>
2018-07-07 23:34:47 -0400

    almost done with make screen

--- core/src/com/fish/notes/MakeScreen.java
+++ core/src/com/fish/notes/MakeScreen.java
@@ -10,9 +10,11 @@ import com.fish.core.notes.LoginResult;
 
 public class MakeScreen extends MyScreen {
     private Stage stage;
-    private TextButton sumbit, picchoice, textchoice;
+    private TextButton submit;
+    private CheckBox picchoice, textchoice;
     private Label label;
-    private TextField title, postdata, classbox;
+    private TextField title, classbox;
+    private TextArea postdata;
     private Notes game;
 
     @Override
@@ -30,7 +32,7 @@ public class MakeScreen extends MyScreen {
         title.setMessageText("Title");
         title.setColor(Color.BLUE);
 
-        this.postdata = new TextField("", Notes.skin);
+        this.postdata = new TextArea("", Notes.skin);
         postdata.setMessageText("Enter Text");
         postdata.setColor(Color.BLUE);
 
@@ -38,23 +40,38 @@ public class MakeScreen extends MyScreen {
         classbox.setMessageText("Class");
         classbox.setColor(Color.BLUE);
 
-        this.picchoice = new TextButton("Picture Post", Notes.skin);
+        this.picchoice = new CheckBox("Picture Post", Notes.skin);
 
-        this.textchoice = new TextButton("Text Post", Notes.skin);
+        this.textchoice = new CheckBox("Text Post", Notes.skin);
 
+
+
+        Table superTable = new Table();
         Table table = new Table();
         table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        table.add(title).width(40).height(40).expand().top().left();
+        table.add(classbox).width(40).height(40).expand().top().right().row();
         table.add(picchoice).width(Gdx.graphics.getWidth()/2).height(30).left();
-        table.add(textchoice).width(Gdx.graphics.getWidth()/2).height(30).right();
-        table.row();
-        table.add(title).width(40).height(40).expand().top().center();
-        table.row();
-        table.add(postdata).width(40).height(40).expand().top().center();
-        table.row();
-        table.add(classbox).width(40).height(40).expand().top().center();
-        table.row();
-        table.add(title).width(40).height(40).expand().top().center();
-        table.row();
+        table.add(textchoice).width(Gdx.graphics.getWidth()/2).height(30).right().row();
+
+        superTable.add(table);
+
+        final Table container = new Table();
+        picchoice.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+
+            }
+        });
+        textchoice.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                container.add(postdata).prefSize(postdata.getWidth(), postdata.getHeight()).row();
+            }
+        });
+        superTable.add(container);
+        stage.addActor(superTable);
+
     }
 
 
