772252fb34982ce7b8a08c3fb4b08bf0524e3c9c
Mahi Nair <mahifreya@gmail.com>
2018-07-08 03:14:43 -0400

    Justins cool thing

--- core/src/com/fish/notes/BoughtScreen.java
+++ core/src/com/fish/notes/BoughtScreen.java
@@ -2,23 +2,23 @@ package com.fish.notes;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.fish.core.notes.Post;
 
 public class BoughtScreen extends MyScreen {
     private Stage stage;
-    private Label username, coins, totalvotes;
-    private Image user, coin, votes;
+    private Label username, coins, totalvotes, votes, title;
+    private Image user, coin, vote;
     private Notes notes;
 
 
-
-    public BoughtScreen(final Notes notes)
-    {
+    public BoughtScreen(final Notes notes) {
         this.stage = new Stage();
         this.notes = notes;
 
@@ -33,7 +33,7 @@ public class BoughtScreen extends MyScreen {
 
         this.user = new Image(new Texture("Fishnotes_udp.png"));
         this.coin = new Image(new Texture("coins.png"));
-        this.votes = new Image(new Texture("Fishnotes_upvote.png"));
+        this.vote = new Image(new Texture("Fishnotes_upvote.png"));
 
         Table table = new Table();
         table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -41,22 +41,36 @@ public class BoughtScreen extends MyScreen {
         table.add(username).height(30).width(30).left();
         table.add(coin).height(30).width(30).right();
         table.add(coins).height(30).width(30).left();
-        table.add(votes).height(30).width(30).right();
+        table.add(vote).height(30).width(30).right();
         table.add(totalvotes).height(30).width(30).left();
         table.row();
 
-        for(int i = 0; i < Backend.getBoughtPosts().size(); i++) {
-            PostScreen item = new PostScreen();
+        for (int i = 0; i < Backend.getBoughtPosts().size(); i++) {
+            this.stage = new Stage();
+            for (Post p : Backend.getBoughtPosts()) {
+                this.title = new Label("" + p.getTitle(), Notes.skin);
+                title.setColor(Color.BLUE);
+                this.votes = new Label("Votes: " + Backend.getUpvotes(p), Notes.skin);
+                votes.setColor(Color.BLUE);
+                this.username = new Label("" + Backend.getAccount(Notes.account.getID()).getUsername(), Notes.skin);
+                Table supertable = new Table();
+                supertable.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+                supertable.add(title).width(40).height(40).expand().left();
+                supertable.add(votes).width(40).height(40).expand().right();
+                supertable.row();
+                supertable.add(username).width(40).height(40).expand().bottom().left();
+                supertable.row();
+                stage.addActor(supertable);
+                Gdx.input.setInputProcessor(stage);
+            }
         }
     }
+        public void render ( float delta){
+            stage.act(delta);
+            stage.draw();
+        }
+        public void dispose () {
+            stage.dispose();
+        }
 
-    @Override
-    public void render(float delta) {
-        stage.act(delta);
-        stage.draw();
-    }
-    @Override
-    public void dispose() {
-        stage.dispose();
-    }
 }
